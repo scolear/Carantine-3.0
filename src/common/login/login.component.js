a4f042cbb3cb5d2bd03cd619195a97853631a1a1
scolear <bobtera@gmail.com>
2020-08-10 11:50:12 +0200

    login credentials sending

--- src/common/login/login.component.js
+++ src/common/login/login.component.js
@@ -3,9 +3,36 @@ import './login.component.less';
 
 const component = function() {
 
-    const controller = [function(){
+    const controller = ['$http' , function($http) {
 
         const ctrl = this;
+
+        ctrl.user = {
+            email: "", 
+            password: ""
+        };
+
+        ctrl.submitLogin = function () 
+        {
+            if (ctrl.user.email != "" && ctrl.user.password != "") {
+                postCommand(ctrl.user);
+            } 
+        };
+
+        const postCommand = function (userInfo) 
+        {
+            userInfo.returnSecureToken = "true";
+
+            $http.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDg6ZazQiE--1GenRyZHwazpj-2OnrG_9o', userInfo)
+            .then(
+                function success(res) {
+                    console.log("Login successful");
+                },
+                function error(res) {
+                    console.log("Login failed.");
+                }
+            );
+        };
 
     }];
 
